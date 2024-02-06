8680cadfe5c05ca1817e5b0fd3f259b22a3fcc5e
praneethmareddy <112878756+praneethmareddy@users.noreply.github.com>
2024-02-06 22:16:42 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const URI = process.env.MONGODB_URI;
 const connectDb = async () => {
   try {
-    await mongoose.connect(URI);
+    await mongoose.connect('mongodb+srv://praneethmareddy:saip9091@cluster0.rfvy63z.mongodb.net/mern?retryWrites=true&w=majority');
     console.log("connection successful to DB");
   } catch (error) {
     console.error("database connection fail");
