8a8ee600801cac2e78983c1f59067838509467f8
praneethmareddy <112878756+praneethmareddy@users.noreply.github.com>
2024-02-07 00:11:36 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -4,7 +4,7 @@ const paymentController = require("../controllers/paymentController.js");
 const router = express.Router();
 
 router.route("/checkout").post(paymentController.checkout);
-
+router.route("/checkoutservice").post(paymentController.checkout1);
 router.route("/paymentverification").post(paymentController.paymentVerification);
 
 module.exports = router;
